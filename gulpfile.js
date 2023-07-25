7b7357fc623d4c8a319926f68ee22ab4870347b9
Mark Michaelis <thragor@mailbox.org>
2023-07-25 21:11:39 +0200

    feat: Optimize Exclude Lists for Gulp ZIP

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,7 @@ var zip = require('gulp-zip');
 gulp.task('zip', function () {
   return gulp.src([
     './**/*', 
-    '!./{.git,.git/**/*}', 
-    '!./{.vscode,.vscode/**/*}', 
-    '!./{.idea,.idea/**/*}', 
+    '!./{.*,.*/**/*}', 
     '!./{node_modules,node_modules/**/*}', 
     '!./assets/{sass,sass/*}', 
     '!./gulpfile.js', 
