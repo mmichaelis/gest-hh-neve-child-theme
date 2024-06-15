b59dfa2623ac52bd8e754fd78955ce6387662783
Mark Michaelis <mark.michaelis@coremedia.com>
2024-06-15 15:21:59 +0200

    ci(release): Again, try to find the artifact

--- sh/release.sh
+++ sh/release.sh
@@ -298,8 +298,12 @@ declare -r releaseHash
 (
   cd build | log_info
   zip add --quiet --recurse-paths -9 --archive-comment "${artifactPath}" . <<< "${type^} release ${releaseVersion} of ${projectName}." | log_info
+  cd .. | log_info
+  # shellcheck disable=SC2012
+  ls -l --human-readable | log_info
+  sizeInfo="$(du --summarize --human-readable "${artifactName}" | cut -f1)"
   declare -r sizeInfo
-  log_info "Created Release Artifact: ${artifactName} (path: ${artifactPath})"
+  log_info "Created Release Artifact: ${artifactName} (path: ${artifactPath}, size: ${sizeInfo})"
 )
 
 # ------------------------------------------------------------------------------
