3f3ae56c773cea7656979189c8cc3d3127b8ce74
Mark Michaelis <mark.michaelis@coremedia.com>
2024-06-16 11:49:54 +0200

    fix(theme): Validate functions.php
    Just checked, that it is still aligned with Neve Child Theme Example.
    Applied some reformatting.

--- src/functions.php
+++ src/functions.php
@@ -1,14 +1,15 @@
-<?php
+<?php /** @noinspection ALL */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
-if ( ! function_exists( 'neve_child_load_css' ) ):
-	/**
-	 * Load CSS file.
-	 */
-	function neve_child_load_css() {
-		wp_enqueue_style( 'neve-child-style', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'neve-style' ), NEVE_VERSION );
-	}
+if (!function_exists('neve_child_load_css')):
+    /**
+     * Load CSS file.
+     */
+    function neve_child_load_css()
+    {
+        wp_enqueue_style('neve-child-style', trailingslashit(get_stylesheet_directory_uri()) . 'style.css', array('neve-style'), NEVE_VERSION);
+    }
 endif;
-add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
+add_action('wp_enqueue_scripts', 'neve_child_load_css', 20);
